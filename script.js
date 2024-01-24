eca7ce9e987647b3a457137a092bed8bf2fee339
Giovani Bontempo <tempogiobon@gmail.com>
2024-01-24 12:17:13 -0300

    estilo css para desktop

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('teste')
+// ao carregar dá o foco no txtarea para começar a digitar
+document.addEventListener("DOMContentLoaded", function() {
+    var textarea = document.getElementById("entrada-txt");
+    textarea.focus();
+    textarea.setSelectionRange(textarea.value.length, textarea.value.length);
+});
